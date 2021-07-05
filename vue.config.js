3c0aaeb4ef2b9992302a1522aacc0949b2151191
15321 <153211656@qq.com>
2021-07-05 16:38:12 +0800

    本地调试的问题

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     proxy: { //配置跨域
       '/api': {
           // target: 'http://127.0.0.1:9999/api', //本地调试时可以用这个，也可以用下一个
-          target: 'http://10.2.4.36:80/api', //部署上线时一定要用这个
+          target: 'http://10.2.4.36:80/api', //本地调试时也可以用这个
           // target: 'http://10.2.4.36:9999/api', //部署上线时一定要用这个
 
           ws: true,
